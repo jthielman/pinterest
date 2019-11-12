20b735164e1660a63c20acd0d0c4e5cd5041a003
John Thielman <john.thielman@gmail.com>
2019-11-11 18:51:15 -0600

    board refreshes after deleting pin

--- src/javascripts/components/SingleBoard/singleBoard.js
+++ src/javascripts/components/SingleBoard/singleBoard.js
@@ -1,28 +1,20 @@
 import $ from 'jquery';
-import axios from 'axios';
 import pinsData from '../../helpers/data/pinsData';
 
 import './singleBoard.scss';
 import utilities from '../../helpers/utilities';
-import apiKeys from '../../helpers/apiKeys.json';
-
-const baseUrl = apiKeys.firebasekeys.databaseURL;
 
 const deletePin = (e) => {
   e.stopImmediatePropagation();
   const pinIdToDelete = e.target.id.split('delete-')[1];
-  axios.get(`${baseUrl}/pins/${pinIdToDelete}.json`)
-    .then((response) => {
-      console.log('pin', response);
-    })
-    .catch((error) => console.error(error));
 
-  pinsData.deletePin(pinIdToDelete);
-  // .then((boardIdOfDeletedPin) => {
-  //   // eslint-disable-next-line no-use-before-define
-  //   showOneBoard(boardIdOfDeletedPin);
-  // })
-  // .catch((err) => console.error(err));
+  pinsData.deletePin(pinIdToDelete)
+    .then(() => {
+      const boardIdOfDeletedPin = $(e.target).attr('boardInfo');
+      // eslint-disable-next-line no-use-before-define
+      showOneBoard(boardIdOfDeletedPin);
+    })
+    .catch((err) => console.error(err));
 };
 
 const backToBoards = (e) => {
@@ -34,7 +26,6 @@ const backToBoards = (e) => {
 const showOneBoard = (boardId) => {
   pinsData.getPinsByBoardId(boardId)
     .then((pins) => {
-      // console.log('from showOneBoard on singleBoard.js', pins);
       let string = `
         <div class="row justify-content-between">
           <h1>Pins</h1>
@@ -50,7 +41,7 @@ const showOneBoard = (boardId) => {
             <p class="card-text">${pin.description}</p>
           </div>
           <div class="card-footer">
-          <button class="btn btn-danger delete-pin" id="delete-${pin.id}">Delete pin</button>
+          <button class="btn btn-danger delete-pin" id="delete-${pin.id}" boardInfo="${pin.boardId}">Delete pin</button>
           </div>
         </div>
       `;
